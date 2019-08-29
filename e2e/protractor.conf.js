c5cb291caff8bbda6a7209f04fa0a5e6d115e0e2
paflov <paf@titelfrei.de>
2019-08-29 12:18:36 +0200

    next try for ci setup

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'firefox'
+    'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
